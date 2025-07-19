d0831ba0f35bf60a19fe3775979e97f95c0d14d1
mrchidubem <mrchidubem8@gmail.com>
2025-07-19 17:33:20 +0100

    deleted files

--- testcase.py
+++
@@ -1,6 +0,0 @@
-print("this is the first line of my project ")
-print("this is the second line of my project")
-print("this is the third line of my project ")
-print("this is the third line of my project")
-print("fifth line")
-x = 4
